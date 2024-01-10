010170d7b258f936eb6dc5c3728a467de63bbdb3
lethanhlong47 <78028582+lethanhlong47@users.noreply.github.com>
2024-01-10 12:40:13 +0700

    Update Practical 2.sql

--- Practical 2.sql
+++ Practical 2.sql
@@ -35,3 +35,43 @@ GROUP BY user_id
 HAVING COUNT(post_id)>1;
 
 --ex7: datalemur-cards-issued-difference. 
+SELECT  card_name,
+        MAX(issued_amount)-MIN(issued_amount) as soluong
+FROM monthly_cards_issued
+GROUP BY card_name
+ORDER BY MAX(issued_amount)-MIN(issued_amount) DESC;
+
+--ex8: datalemur-non-profitable-drugs.
+SELECT
+  manufacturer,
+  COUNT(drug) AS drug_count, 
+  SUM(cogs - total_sales) AS total_loss
+FROM pharmacy_sales
+WHERE cogs > total_sales
+GROUP BY manufacturer
+ORDER BY total_loss DESC;
+
+--ex9: leetcode-not-boring-movies.
+Select *
+From Cinema
+Where id%2 <>0
+And description not like '%boring%'
+Order by id DESC
+
+--ex10: leetcode-number-of-unique-subject.
+Select  teacher_id,
+        count(distinct subject_id) as cnt
+From Teacher
+group by teacher_id
+
+--ex11: leetcode-find-followers-count.
+Select  user_id,
+        count(follower_id) as followers_count
+From Followers
+Group by user_id
+
+--ex12:leetcode-classes-more-than-5-students.
+Select class
+From Courses
+Group by class
+Having count(student) >=5
